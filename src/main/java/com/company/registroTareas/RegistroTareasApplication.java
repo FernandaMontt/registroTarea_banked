6fad8319757388c0cd5b255487760eef12fc8edc
Fernanda Montt Muñoz <mfmontt@sacyr.com>
2023-03-27 17:40:41 -0300

    mantenedor de registro de tareas

---
+++ src/main/java/com/company/registroTareas/RegistroTareasApplication.java
@@ -0,0 +1,13 @@
+package com.company.registroTareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/registroTareas/RegistroTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.registroTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
